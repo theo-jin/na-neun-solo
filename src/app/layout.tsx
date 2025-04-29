31581db0509939d4b53c283cbe4ce77e3dfec73d
Theo Jin <jhb_august@naver.com>
2025-04-29 22:57:16 +0900

    feat:auth.js is set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
+import type { Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: '나는 솔로 이상형 월드컵',
   description: '나는 솔로 이상형 월드컵',
-  viewport:
-    'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+};
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 const notoSansKr = Noto_Sans_KR({
   weight: ['500'],
